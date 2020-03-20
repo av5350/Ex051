a0e7e8ff779f9736bd31bbb0779ced9b0ff2f741
av5350@bs.amalnet.k12.il <itayw5350>
2020-03-20 19:18:29 +0200

    end code

--- app/src/main/java/com/example/ex051/MainActivity.java
+++ app/src/main/java/com/example/ex051/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ex051;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     Random rnd = new Random();
 
-    int correctAnswers = 0;
+    int correctAnswers = 0, level3Limit = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,68 +59,85 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void level1Check(View view) {
-        String sum = input1.getText().toString();
-        if (sum.isEmpty())
-        {
-            Toast.makeText(this, "Invalid input - try again", Toast.LENGTH_SHORT).show();
+        if (num4.getText().toString().equals("num4")) {
+            String sum = input1.getText().toString();
+            if (sum.isEmpty()) {
+                Toast.makeText(this, "Invalid input - try again", Toast.LENGTH_SHORT).show();
+            } else {
+                if (Integer.parseInt(sum) == Integer.parseInt(num1.getText().toString()) + Integer.parseInt(num2.getText().toString())) {
+                    successIV1.setImageResource(R.drawable.button_true);
+                    correctAnswers++;
+                } else {
+                    successIV1.setImageResource(R.drawable.button_false);
+                }
+                num3.setText(Integer.parseInt(num1.getText().toString()) + Integer.parseInt(num2.getText().toString()) + "");
+                if (num4.getText().toString().equals("num4")) {
+                    num4.setText(rnd.nextInt(90) + 10 + "");
+                }
+            }
         }
         else {
-            if (Integer.parseInt(sum) == Integer.parseInt(num1.getText().toString()) + Integer.parseInt(num2.getText().toString()))
-            {
-                successIV1.setImageResource(R.drawable.button_true);
-                correctAnswers = 1;
-            }
-            else {
-                successIV1.setImageResource(R.drawable.button_false);
-            }
-            num3.setText(Integer.parseInt(num1.getText().toString()) + Integer.parseInt(num2.getText().toString()) + "");
-            num4.setText(rnd.nextInt(90) + 10 + "");
+            Toast.makeText(this, "You've already answered this level!", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void level2Check(View view) {
-        String sum = input2.getText().toString();
-        if (sum.isEmpty())
-        {
-            Toast.makeText(this, "Invalid input - try again", Toast.LENGTH_SHORT).show();
+        if (num6.getText().toString().equals("num6")) {
+            if (num3.getText().toString() == "num3" && num4.getText().toString() == "num4") {
+                Toast.makeText(this, "Invalid level - try again", Toast.LENGTH_SHORT).show();
+            } else {
+                String sum = input2.getText().toString();
+                if (sum.isEmpty()) {
+                    Toast.makeText(this, "Invalid input - try again", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (Integer.parseInt(sum) == Integer.parseInt(num3.getText().toString()) + Integer.parseInt(num4.getText().toString())) {
+                        successIV2.setImageResource(R.drawable.button_true);
+                        correctAnswers++;
+                    }
+                    else {
+                        successIV2.setImageResource(R.drawable.button_false);
+                    }
+                    num5.setText(Integer.parseInt(num3.getText().toString()) + Integer.parseInt(num4.getText().toString()) + "");
+                    if (num6.getText().toString().equals("num6")) {
+                        num6.setText(rnd.nextInt(90) + 10 + "");
+                    }
+                }
+            }
         }
         else {
-            if (Integer.parseInt(sum) == Integer.parseInt(num3.getText().toString()) + Integer.parseInt(num4.getText().toString()))
-            {
-                successIV2.setImageResource(R.drawable.button_true);
-                correctAnswers = 2;
-            }
-            else {
-                successIV2.setImageResource(R.drawable.button_false);
-            }
-            num5.setText(Integer.parseInt(num3.getText().toString()) + Integer.parseInt(num4.getText().toString()) + "");
-            num6.setText(rnd.nextInt(90) + 10 + "");
+            Toast.makeText(this, "You've already answered this level!", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void level3Check(View view) {
-        String sum = input3.getText().toString();
-        if (sum.isEmpty())
-        {
-            Toast.makeText(this, "Invalid input - try again", Toast.LENGTH_SHORT).show();
+        if (level3Limit == 1) {
+            if (num5.getText().toString() == "num5" && num6.getText().toString() == "num6") {
+                Toast.makeText(this, "Invalid level - try again", Toast.LENGTH_SHORT).show();
+            } else {
+                String sum = input3.getText().toString();
+                if (sum.isEmpty()) {
+                    Toast.makeText(this, "Invalid input - try again", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (Integer.parseInt(sum) == Integer.parseInt(num5.getText().toString()) + Integer.parseInt(num6.getText().toString())) {
+                        successIV3.setImageResource(R.drawable.button_true);
+                        correctAnswers++;
+                    } else {
+                        successIV3.setImageResource(R.drawable.button_false);
+                    }
+                    Toast.makeText(this, correctAnswers + "/3", Toast.LENGTH_SHORT).show();
+                }
+            }
+            level3Limit = 0;
         }
         else {
-            if (Integer.parseInt(sum) == Integer.parseInt(num5.getText().toString()) + Integer.parseInt(num6.getText().toString()))
-            {
-                successIV3.setImageResource(R.drawable.button_true);
-                correctAnswers = 3;
-            }
-            else {
-                successIV3.setImageResource(R.drawable.button_false);
-            }
-            Toast.makeText(this, correctAnswers + "/3", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "You've already answered this level!", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void clearGame(View view) {
         correctAnswers = 0;
-        num1.setText("num1");
-        num2.setText("num2");
+        num1.setText(rnd.nextInt(90) + 10 + "");
+        num2.setText(rnd.nextInt(90) + 10 + "");
         num3.setText("num3");
         num4.setText("num4");
         num5.setText("num5");
@@ -126,6 +146,8 @@ public class MainActivity extends AppCompatActivity {
         input1.setText("");
         input2.setText("");
         input3.setText("");
+
+        level3Limit = 1;
 
         successIV1.setImageResource(R.drawable.button_unknown);
         successIV2.setImageResource(R.drawable.button_unknown);
